f06813ed93730e71419d2ccb027cd8adc4d9ac38
JuniorArias02 <junior.arias02yt@gmail.com>
2025-01-16 09:49:45 -0500

    nuevas img

--- app-files/data.js
+++ app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-lago-y-salon-comunal-1",
-      "name": "LAGO Y SALON COMUNAL-1",
+      "id": "0-13---13",
+      "name": "13 - 13",
       "levels": [
         {
           "tileSize": 256,
@@ -18,25 +18,18 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 750,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.935165649200779,
-          "pitch": 0.023755368903245255,
-          "rotation": 0,
-          "target": "1-lago-y-zona-de-hamacas"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "1-lago-y-zona-de-hamacas",
-      "name": "LAGO Y ZONA DE HAMACAS",
+      "id": "1-12---12",
+      "name": "12 - 12",
       "levels": [
         {
           "tileSize": 256,
@@ -52,162 +45,163 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 1000,
+      "faceSize": 750,
       "initialViewParameters": {
-        "yaw": 2.778339683742977,
-        "pitch": 0.13498602460291664,
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-11---11",
+      "name": "11 - 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-10---10",
+      "name": "10 - 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-9---9",
+      "name": "9 - 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-8---8",
+      "name": "8 - 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7---7",
+      "name": "7 - 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.47079225637235744,
+        "pitch": 0.08292924338192087,
         "fov": 1.3649120070492948
       },
       "linkHotspots": [
         {
-          "yaw": 1.9042799066424028,
-          "pitch": -0.04275957931571739,
+          "yaw": -0.5762784436847301,
+          "pitch": 0.21205128058032408,
           "rotation": 0,
-          "target": "1-lago-y-zona-de-hamacas"
-        },
-        {
-          "yaw": -2.662646212018654,
-          "pitch": 0.06596727480885889,
-          "rotation": 0,
-          "target": "0-lago-y-salon-comunal-1"
+          "target": "9-4---4"
         }
       ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-3---3",
-      "name": "3 - 3",
-      "levels": [
+      "infoHotspots": [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
+          "yaw": 0.11827947094637103,
+          "pitch": 0.26427843830414766,
+          "title": "Entrada",
+          "text": "<br>"
         }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-1---1",
-      "name": "1 - 1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-2---2",
-      "name": "2 - 2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-4---4",
-      "name": "4 - 4",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-5---5",
-      "name": "5 - 5",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
+      ]
     },
     {
       "id": "7-6---6",
@@ -237,8 +231,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "8-7---7",
-      "name": "7 - 7",
+      "id": "8-5---5",
+      "name": "5 - 5",
       "levels": [
         {
           "tileSize": 256,
@@ -264,8 +258,61 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "9-8---8",
-      "name": "8 - 8",
+      "id": "9-4---4",
+      "name": "4 - 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.115897340420034,
+          "pitch": -0.3402030490058543,
+          "rotation": 0,
+          "target": "6-7---7"
+        },
+        {
+          "yaw": -1.919524716597559,
+          "pitch": -0.05039419305323456,
+          "rotation": 0,
+          "target": "14-lago-y-zona-de-hamacas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1228702795626013,
+          "pitch": -0.2137893285877155,
+          "title": "Retroceder",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.033890248339624,
+          "pitch": 0.14571101516493812,
+          "title": "Entrar",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "10-2---2",
+      "name": "2 - 2",
       "levels": [
         {
           "tileSize": 256,
@@ -291,8 +338,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "10-9---9",
-      "name": "9 - 9",
+      "id": "11-1---1",
+      "name": "1 - 1",
       "levels": [
         {
           "tileSize": 256,
@@ -318,8 +365,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "11-10---10",
-      "name": "10 - 10",
+      "id": "12-3---3",
+      "name": "3 - 3",
       "levels": [
         {
           "tileSize": 256,
@@ -345,8 +392,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "12-11---11",
-      "name": "11 - 11",
+      "id": "13-lago-y-salon-comunal-1",
+      "name": "LAGO Y SALON COMUNAL-1",
       "levels": [
         {
           "tileSize": 256,
@@ -362,18 +409,37 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 750,
+      "faceSize": 1000,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.39539762016416624,
+          "pitch": 0.003883180400794828,
+          "rotation": 0,
+          "target": "5-8---8"
+        },
+        {
+          "yaw": -2.0552324409007454,
+          "pitch": -0.011900487784000546,
+          "rotation": 0,
+          "target": "14-lago-y-zona-de-hamacas"
+        },
+        {
+          "yaw": 2.8620276228952015,
+          "pitch": 0.09316082002031933,
+          "rotation": 0,
+          "target": "0-13---13"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "13-12---12",
-      "name": "12 - 12",
+      "id": "14-lago-y-zona-de-hamacas",
+      "name": "LAGO Y ZONA DE HAMACAS",
       "levels": [
         {
           "tileSize": 256,
@@ -389,40 +455,32 @@ var APP_DATA = {
           "size": 1024
         }
       ],
-      "faceSize": 750,
+      "faceSize": 1000,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "14-13---13",
-      "name": "13 - 13",
-      "levels": [
+      "linkHotspots": [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "yaw": -2.199346620892305,
+          "pitch": 0.09157081293128932,
+          "rotation": 0,
+          "target": "8-5---5"
         },
         {
-          "tileSize": 512,
-          "size": 512
+          "yaw": -0.03216303902386919,
+          "pitch": 0.22749286911733257,
+          "rotation": 0,
+          "target": "3-10---10"
         },
         {
-          "tileSize": 512,
-          "size": 1024
+          "yaw": 1.9030521477735087,
+          "pitch": 0.015084635910570299,
+          "rotation": 0,
+          "target": "13-lago-y-salon-comunal-1"
         }
       ],
-      "faceSize": 750,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
@@ -430,7 +488,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
