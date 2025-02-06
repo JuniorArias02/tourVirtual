cc5cebbff03fafb1c36d0b17f2c850a7533d188d
JuniorArias02 <junior.arias02yt@gmail.com>
2025-02-06 10:22:18 -0500

    nuevas imagenes v2

--- app-files/data.js
+++ app-files/data.js
@@ -1,7 +1,113 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-1",
+      "id": "0-zona-comercial",
+      "name": "Zona Comercial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "yaw": -0.5705263695132974,
+        "pitch": -0.09090809642121656,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9410426380340162,
+          "pitch": -0.008197336110079334,
+          "rotation": 0,
+          "target": "1-zona-inicial"
+        },
+        {
+          "yaw": -0.3794456806158131,
+          "pitch": -0.004099326364702449,
+          "rotation": 0,
+          "target": "8-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3584873773443,
+          "pitch": 0.13293447950409387,
+          "title": "ZONA LAGO",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-zona-inicial",
+      "name": "Zona Inicial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "yaw": -0.09468429928094757,
+        "pitch": 0.10985427943399273,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12333688403714937,
+          "pitch": -0.019824439078256262,
+          "rotation": 0,
+          "target": "0-zona-comercial"
+        },
+        {
+          "yaw": -1.1136254794839697,
+          "pitch": 0.014778137329528818,
+          "rotation": 0,
+          "target": "12-11"
+        },
+        {
+          "yaw": -2.2696987487751805,
+          "pitch": 0.14090845858479284,
+          "rotation": 0,
+          "target": "7-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.93439044759847,
+          "pitch": 0.163523068671239,
+          "title": "Zona Hamacas",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": 0.0012915015540588826,
+          "pitch": -0.11092664836567856,
+          "title": "ZONA COMERCIAL",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-1",
       "name": "1",
       "levels": [
         {
@@ -20,15 +126,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.13559865727033404,
+        "pitch": 0.00556580182169597,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0468596083495125,
+          "pitch": 0.887770811006332,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8174176294708744,
+          "pitch": 0.8955538853767564,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5615239342361455,
+          "pitch": 0.8923484105980073,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "1-2",
+      "id": "3-2",
       "name": "2",
       "levels": [
         {
@@ -47,15 +172,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.014768170593796981,
+        "pitch": 0.04081588002585512,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.069257752608019,
+          "pitch": 0.9206607735644496,
+          "title": "inmobiliariasequoia",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -2.817144528629541,
+          "pitch": 0.9072867642268516,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.547183969619372,
+          "pitch": 0.911828593035036,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "2-3",
+      "id": "4-3",
       "name": "3",
       "levels": [
         {
@@ -74,15 +218,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0769658461485854,
+          "pitch": 0.9110873797325443,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.818400946697812,
+          "pitch": 0.9042631194138444,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.547257120657653,
+          "pitch": 0.8991103252852017,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "3-4",
+      "id": "5-4",
       "name": "4",
       "levels": [
         {
@@ -101,15 +264,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.014764491655276046,
+        "pitch": 0.016694863526826254,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0597343838501825,
+          "pitch": 0.8982999021797635,
+          "title": "inmobiliariasequoia",
+          "text": ""
+        },
+        {
+          "yaw": -2.8068047823167177,
+          "pitch": 0.8905696976317117,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.560152917481293,
+          "pitch": 0.9023982870016507,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "4-5",
+      "id": "6-5",
       "name": "5",
       "levels": [
         {
@@ -128,15 +310,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.026851219261454062,
+        "pitch": 0.018552672739030385,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.059950539460001,
+          "pitch": 0.899414709518096,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8233748348518706,
+          "pitch": 0.8959020159228821,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5353159767397724,
+          "pitch": 0.8972423666557638,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "5-6",
+      "id": "7-6",
       "name": "6",
       "levels": [
         {
@@ -159,11 +360,37 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": -0.01332265752139783,
+          "pitch": 0.5016399607894186,
+          "rotation": 0,
+          "target": "1-zona-inicial"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.053210879093939,
+          "pitch": 0.8932726949114738,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.807809951335523,
+          "pitch": 0.8983707688769282,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5561296018712785,
+          "pitch": 0.9015024282195618,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<div><br></div>"
+        }
+      ]
     },
     {
-      "id": "6-7",
+      "id": "8-7",
       "name": "7",
       "levels": [
         {
@@ -182,15 +409,47 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.04564707274446178,
+        "pitch": -0.04081588002584624,
+        "fov": 1.38217411905719
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": 1.2327484292984412,
+          "pitch": 0.050311137377004655,
+          "rotation": 0,
+          "target": "0-zona-comercial"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2371429219595989,
+          "pitch": -0.15548327638738968,
+          "title": "ATRAS",
+          "text": "<br>"
+        },
+        {
+          "yaw": -3.0601332958526193,
+          "pitch": 0.9024870574718271,
+          "title": "inmobiliariasequoia",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -2.8017035617306014,
+          "pitch": 0.9063066831892606,
+          "title": "inmobiliariasequoia",
+          "text": "<div><br></div>"
+        },
+        {
+          "yaw": -2.550494096898671,
+          "pitch": 0.9013641493251594,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "7-8",
+      "id": "9-8",
       "name": "8",
       "levels": [
         {
@@ -209,15 +468,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0504250702310376,
+          "pitch": 0.893813543611266,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8098060963083284,
+          "pitch": 0.8922057958698524,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5561600724640385,
+          "pitch": 0.8927388251208157,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "8-9",
+      "id": "10-9",
       "name": "9",
       "levels": [
         {
@@ -236,15 +514,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0515002191424916,
+          "pitch": 0.8995674480528812,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.810490443311366,
+          "pitch": 0.8970926969148998,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.544149342188181,
+          "pitch": 0.8915751651866479,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "9-10",
+      "id": "11-10",
       "name": "10",
       "levels": [
         {
@@ -263,15 +560,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.0469552654017633,
+          "pitch": 0.8933344778767864,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.5565535055911095,
+          "pitch": 0.8902586390907494,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.808889536858004,
+          "pitch": 0.8931767722210484,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "10-11",
+      "id": "12-11",
       "name": "11",
       "levels": [
         {
@@ -290,15 +606,47 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+        {
+          "yaw": 0.1368159370860056,
+          "pitch": 0.08994948110527545,
+          "rotation": 0,
+          "target": "1-zona-inicial"
+        },
+        {
+          "yaw": 0.6689382570751459,
+          "pitch": 0.06436485154999261,
+          "rotation": 0,
+          "target": "7-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0571210744346757,
+          "pitch": 0.8905570798461078,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8084726806324944,
+          "pitch": 0.8896348307633417,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.547804527037341,
+          "pitch": 0.8903515013764469,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "11-12",
+      "id": "13-12",
       "name": "12",
       "levels": [
         {
@@ -317,15 +665,34 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -3.053825098436967,
+          "pitch": 0.8856762682399975,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.8051862848678013,
+          "pitch": 0.885200550491783,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.55028423570508,
+          "pitch": 0.8854435284226341,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
+        }
+      ]
     },
     {
-      "id": "12-13",
+      "id": "14-13",
       "name": "13",
       "levels": [
         {
@@ -344,58 +711,31 @@ var APP_DATA = {
       ],
       "faceSize": 900,
       "initialViewParameters": {
-        "pitch": 0,
         "yaw": 0,
-        "fov": 1.5707963267948966
+        "pitch": 0,
+        "fov": 1.38217411905719
       },
       "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "13-14",
-      "name": "14",
-      "levels": [
+      "infoHotspots": [
         {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "yaw": -3.069447313766375,
+          "pitch": 0.8971627018653887,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
         },
         {
-          "tileSize": 512,
-          "size": 512
-        }
-      ],
-      "faceSize": 400,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "14-15",
-      "name": "15",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
+          "yaw": -2.8122772263162013,
+          "pitch": 0.8917734946782652,
+          "title": "inmobiliariasequoia",
+          "text": "<br>"
         },
         {
-          "tileSize": 512,
-          "size": 512
+          "yaw": -2.5576538744791932,
+          "pitch": 0.8935509417686571,
+          "title": "@sequoia.inmobiliaria",
+          "text": "<br>"
         }
-      ],
-      "faceSize": 400,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
+      ]
     }
   ],
   "name": "Project Title",
